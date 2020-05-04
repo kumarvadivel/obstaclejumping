c1d6f60f3f0b9b92e9976457dfbf471cba7ca0bf
kumarvadivel <35343907+kumarvadivel@users.noreply.github.com>
2020-05-04 21:32:23 +0530

    last

--- js/app.js
+++ js/app.js
@@ -4,20 +4,20 @@ var highscore = 0;
 var obsheight = 20;
 var obswidth = 5;
 var obsleft = 5;
-var animaterand = 5000; //decrement upto 3000
-var createElementrand = 1500; //decrement upto 500
+var animaterand = 4000;
+var createElementrand = 1500;
+var u, u1;
 
 var screenheight = screen.height;
 var screenwidth = screen.width
-    //console.log(screenheight, screenwidth);
-    //selectors
+
 const currentscore = document.querySelector('.cur-scr');
 const highscor = document.querySelector('.high-scr');
 var char = document.querySelector(".dino");
 var child = document.querySelector(".sky");
 
 function randomizer() {
-    if (animaterand < 2500) {
+    if (animaterand < 2000) {
         clearInterval(u);
     } else {
         animaterand = animaterand - 100;
@@ -36,19 +36,26 @@ function randomizer1() {
 
 }
 
+var keyhold = 0;
 //eventlisteners
 window.addEventListener("click", gamestart, { once: true });
+window.addEventListener("click", jumpstart, { once: false });
+window.addEventListener("keydown", jumpstart, { once: false });
 
-window.addEventListener("keydown", jump);
-window.addEventListener("keyup", jump1);
-//window.addEventListener("keypress", jump2);
+function jumpstart(e) {
+
+    $(".dino").animate({ top: '40vh' }, 200, 'swing');
+    $(".dino").animate({ top: '60vh' }, 400, 'swing');
+}
+
+
 
 function gamestart(e) {
 
-    var u = setInterval(randomizer, 5000);
-    var u1 = setInterval(randomizer1, 5000);
+    u = setInterval(randomizer, 5000);
+    u1 = setInterval(randomizer1, 5000);
 
-    window.addEventListener('click', jumpstart);
+
     jumpstart();
 
     createelement();
@@ -64,12 +71,6 @@ function gamestart(e) {
 
 
 }
-
-function jumpstart() {
-    $(".dino").animate({ top: '40vh' }, 200);
-    $(".dino").animate({ top: '60vh' }, 200);
-}
-
 
 
 
@@ -104,23 +105,16 @@ function createelement() {
     obs.setAttribute('class', 'obstacle');
     child.appendChild(obs);
 
-    $('.obstacle').animate({ left: "-5vw" }, animaterand, function() {
+    $('.obstacle').animate({ left: "-5vw" }, animaterand, 'linear', function() {
 
         $(this).remove();
 
-        //j = setTimeout(createelement, Math.floor(Math.random() * (1000 - 100 + 1) + 100));
 
     });
 
 
     j = setTimeout(createelement, Math.floor(Math.random() * (3000 - 1000 + 1) + createElementrand));
 
-    //  $('.obstacle').css({ left: "105%" });
-
-    // obs.remove();
-    //console.log('r');
-    //  var t = setInterval(collisioncheck, 500);
-
 
 
 }
@@ -128,32 +122,8 @@ function createelement() {
 
 
 
-function jump(e) {
-
-    if (e.which == 32) {
-
-        $(".dino").animate({ top: '-=6vh' }, 10, 'easeInSine');
-        // $(".dino").animate({ top: '60vh' }, 400);
-    }
 
 
-}
-
-function jump2(e) {
-
-
-    if (e.which == 32) {
-        $(".dino").animate({ top: '40vh' }, 200);
-        $(".dino").animate({ top: '60vh' }, 400);
-    }
-
-
-}
-
-
-function jump1() {
-    $(".dino").animate({ top: '60vh' }, 100, 'easeInSine');
-}
 
 
 
@@ -161,12 +131,7 @@ function jump1() {
 
 
 function collisioncheck() {
-    //  var widhcal = parseInt($('.obstacle').css('left'));
-    //var widthi = Math.round((widhcal / screenwidth) * 100);
-    //var topcal = parseInt($('.dino').css('top'));
-    //var topthi = Math.round((topcal / screenheight) * 100);
-    //console.log("obstacle:" + parseInt($('.obstacle').css('left')));
-    //console.log("player" + topcal + "%");
+
     var w = window.innerWidth;
     var h = window.innerHeight;
 
@@ -178,34 +143,21 @@ function collisioncheck() {
     var e = Math.round(a.top);
     var vl = Math.round((c / w) * 100);
     var vl1 = Math.round((e / h) * 100);
-    //  console.log(vl);
-    // alert(vl1 + "vh");
+
     if (vl >= 8 && vl <= 10) {
         if (vl1 <= 60 && vl1 >= 50) {
 
             alert("gameover\nyourscore:" + currscore);
-            // console.log("out", d);
+
             window.location.reload();
             clearInterval(t);
             clearTimeout(j);
-            // highscore();
+
 
         }
     }
 
 
-    //if (c < 149 && c > 109 && e > 109) {
-    //  alert("collisi");
-    //}
 
 
-    //console.log(c);
-
-    // console.log($(".obstacle").css("left"));
-    // console.log("near");
 }
-//collisioncheck();
-//console.log(a.top, a.left);
-
-// //this line will stop moving elements horizontally
-// //this line will stop the
